9e06248b5a8cffc3dba6cd9a28a843c61bf58b0d
ayo <oliverawebster09@gmail.com>
2026-08-28 19:38:24 +0100

    Add files via upload

---
+++ servicedllhijack.ps1
@@ -0,0 +1,194 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
+$servicesPath = "HKLM:\SYSTEM\CurrentControlSet\Services"
+$services = Get-ChildItem -Path $servicesPath
+$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$timeThreshold = (Get-Date).AddMinutes(-60)
+
+$knownMappings = @{
+    "AarSvc" = "AarSvc.dll"; "AppHostSvc" = "apphostsvc.dll"; "AppIDSvc" = "appidsvc.dll"; "AppMgmt" = "appmgmts.dll";
+    "AppReadiness" = "AppReadiness.dll"; "AppXSvc" = "appxdeploymentserver.dll"; "AudioEndpointBuilder" = "AudioEndpointBuilder.dll";
+    "Audiosrv" = "Audiosrv.dll"; "autotimesvc" = "autotimesvc.dll"; "AxInstSV" = "AxInstSV.dll";
+    "BcastDVRUserService" = "BcastDVRUserService.dll"; "BDESVC" = "bdesvc.dll"; "BFE" = "bfe.dll"; "BITS" = "qmgr.dll";
+    "BluetoothUserService" = "Microsoft.Bluetooth.UserService.dll"; "BrokerInfrastructure" = "psmsrv.dll";
+    "BTAGService" = "BTAGService.dll"; "BthAvctpSvc" = "BthAvctpSvc.dll"; "bthserv" = "bthserv.dll";
+    "camsvc" = "CapabilityAccessManager.dll"; "CaptureService" = "CaptureService.dll"; "cbdhsvc" = "cbdhsvc.dll";
+    "CDPSvc" = "CDPSvc.dll"; "CDPUserSvc" = "CDPUserSvc.dll"; "CertPropSvc" = "certprop.dll"; "ClipSVC" = "ClipSVC.dll";
+    "ConsentUxUserSvc" = "ConsentUxClient.dll"; "CoreMessagingRegistrar" = "coremessaging.dll"; "CscService" = "cscsvc.dll";
+    "DcomLaunch" = "rpcss.dll"; "dcsvc" = "dcsvc.dll"; "defragsvc" = "defragsvc.dll";
+    "DeviceAssociationBrokerSvc" = "deviceaccess.dll"; "DeviceAssociationService" = "das.dll"; "DeviceInstall" = "umpnpmgr.dll";
+    "DevicePickerUserSvc" = "Windows.Devices.Picker.dll"; "DevicesFlowUserSvc" = "DevicesFlowBroker.dll";
+    "DevQueryBroker" = "DevQueryBroker.dll"; "diagsvc" = "DiagSvc.dll"; "DiagTrack" = "diagtrack.dll";
+    "DispBrokerDesktopSvc" = "DispBroker.Desktop.dll"; "DisplayEnhancementService" = "Microsoft.Graphics.Display.DisplayEnhancementService.dll";
+    "DmEnrollmentSvc" = "Windows.Internal.Management.dll"; "dmwappushservice" = "dmwappushsvc.dll"; "Dnscache" = "dnsrslvr.dll";
+    "DsmSvc" = "DeviceSetupManager.dll"; "DsSvc" = "DsSvc.dll"; "DusmSvc" = "dusmsvc.dll"; "Eaphost" = "eapsvc.dll";
+    "EFS" = "efssvc.dll"; "embeddedmode" = "embeddedmodesvc.dll"; "EntAppSvc" = "EnterpriseAppMgmtSvc.dll";
+    "EventLog" = "wevtsvc.dll"; "EventSystem" = "es.dll"; "FontCache" = "FntCache.dll"; "FrameServer" = "FrameServer.dll";
+    "gpsvc" = "gpsvc.dll"; "GraphicsPerfSvc" = "GraphicsPerfSvc.dll"; "HomeGroupListener" = "ListSvc.dll";
+    "HomeGroupProvider" = "provsvc.dll"; "HvHost" = "hvhostsvc.dll"; "icssvc" = "tetheringservice.dll"; "IKEEXT" = "ikeext.dll";
+    "InstallService" = "InstallService.dll"; "iphlpsvc" = "iphlpsvc.dll"; "KeyIso" = "keyiso.dll"; "KtmRm" = "msdtckrm.dll";
+    "LanmanServer" = "srvsvc.dll"; "LanmanWorkstation" = "wkssvc.dll"; "lfsvc" = "lfsvc.dll"; "LicenseManager" = "LicenseManagerSvc.dll";
+    "lmhosts" = "lmhsvc.dll"; "LSM" = "lsm.dll"; "LxpSvc" = "LanguageOverlayServer.dll"; "MapsBroker" = "moshost.dll";
+    "McpManagementService" = "McpManagementService.dll"; "MessagingService" = "MessagingService.dll"; "mpssvc" = "mpssvc.dll";
+    "MSiSCSI" = "iscsiexe.dll"; "NaturalAuthentication" = "NaturalAuth.dll"; "NcdAutoSetup" = "NcdAutoSetup.dll";
+    "Netlogon" = "netlogon.dll"; "netprofm" = "netprofmsvc.dll"; "NetSetupSvc" = "NetSetupSvc.dll"; "NgcCtnrSvc" = "NgcCtnrSvc.dll";
+    "NgcSvc" = "ngcsvc.dll"; "NlaSvc" = "netprofmsvc.dll"; "OneSyncSvc" = "APHostService.dll"; "p2pimsvc" = "pnrpsvc.dll";
+    "PcaSvc" = "pcasvc.dll"; "PhoneSvc" = "PhoneService.dll"; "PimIndexMaintenanceSvc" = "PimIndexMaintenance.dll";
+    "PlugPlay" = "umpnpmgr.dll"; "PolicyAgent" = "ipsecsvc.dll"; "Power" = "umpo.dll"; "PrintNotify" = "PrintConfig.dll";
+    "PrintWorkflowUserSvc" = "PrintWorkflowService.dll"; "ProfSvc" = "profsvc.dll"; "PushToInstall" = "PushToInstall.dll";
+    "RasAuto" = "rasauto.dll"; "RasMan" = "rasmans.dll"; "RemoteAccess" = "mprdim.dll"; "RemoteRegistry" = "regsvc.dll";
+    "RetailDemo" = "RDXService.dll"; "RmSvc" = "RMapi.dll"; "RpcEptMapper" = "RpcEpMap.dll"; "RpcSs" = "rpcss.dll";
+    "SCardSvr" = "SCardSvr.dll"; "ScDeviceEnum" = "ScDeviceEnum.dll"; "Schedule" = "schedsvc.dll"; "SCPolicySvc" = "certprop.dll";
+    "SEMgrSvc" = "SEMgrSvc.dll"; "SensorService" = "SensorService.dll"; "SensrSvc" = "sensrsvc.dll"; "SessionEnv" = "sessenv.dll";
+    "SharedAccess" = "ipnathlp.dll"; "SharedRealitySvc" = "SharedRealitySvc.dll"; "ShellHWDetection" = "shsvcs.dll";
+    "shpamsvc" = "Windows.SharedPC.AccountManager.dll"; "smphost" = "smphost.dll"; "SmsRouter" = "SmsRouterSvc.dll";
+    "SSDPSRV" = "ssdpsrv.dll"; "StateRepository" = "windows.staterepository.dll"; "stisvc" = "wiaservc.dll";
+    "StorSvc" = "storsvc.dll"; "swprv" = "swprv.dll"; "SysMain" = "sysmain.dll"; "TabletInputService" = "TabSvc.dll";
+    "TapiSrv" = "tapisrv.dll"; "TermService" = "termsrv.dll"; "Themes" = "themeservice.dll"; "TimeBrokerSvc" = "TimeBrokerServer.dll";
+    "TokenBroker" = "TokenBroker.dll"; "TroubleshootingSvc" = "MitigationClient.dll"; "tzautoupdate" = "tzautoupdate.dll";
+    "UdkUserSvc" = "windowsudkservices.shellcommon.dll"; "UmRdpService" = "umrdp.dll"; "UnistoreSvc" = "unistore.dll";
+    "upnphost" = "upnphost.dll"; "UserDataSvc" = "userdataservice.dll"; "UserManager" = "usermgr.dll"; "UsoSvc" = "usosvc.dll";
+    "VacSvc" = "vac.dll"; "VaultSvc" = "vaultsvc.dll"; "vmicguestinterface" = "icsvc.dll"; "vmicheartbeat" = "icsvc.dll";
+    "vmickvpexchange" = "icsvc.dll"; "vmicrdv" = "icsvcext.dll"; "vmicshutdown" = "icsvc.dll"; "vmictimesync" = "icsvc.dll";
+    "vmicvmsession" = "icsvc.dll"; "vmicvss" = "icsvcvss.dll"; "W32Time" = "w32time.dll"; "WaaSMedicSvc" = "WaaSMedicSvc.dll";
+    "WalletService" = "WalletService.dll"; "WarpJITSvc" = "Windows.WARP.JITService.dll"; "WbioSrvc" = "wbiosrvc.dll";
+    "Wcmsvc" = "wcmsvc.dll"; "WebClient" = "webclnt.dll"; "Wecsvc" = "wecsvc.dll"; "wercplsupport" = "wercplsupport.dll";
+    "WerSvc" = "WerSvc.dll"; "WFDSConMgrSvc" = "wfdsconmgrsvc.dll"; "WiaRpc" = "wiarpc.dll"; "WinHttpAutoProxySvc" = "winhttp.dll";
+    "Winmgmt" = "WMIsvc.dll"; "WinRM" = "WsmSvc.dll"; "wisvc" = "flightsettings.dll"; "WlanSvc" = "wlansvc.dll";
+    "wlidsvc" = "wlidsvc.dll"; "WManSvc" = "Windows.Management.Service.dll"; "workfolderssvc" = "workfolderssvc.dll";
+    "WpcMonSvc" = "WpcDesktopMonSvc.dll"; "WPDBusEnum" = "wpdbusenum.dll"; "WpnService" = "WpnService.dll";
+    "WpnUserService" = "WpnUserService.dll"; "wscsvc" = "wscsvc.dll"; "wuauserv" = "wuaueng.dll"; "WwanSvc" = "wwansvc.dll";
+    "XblAuthManager" = "XblAuthManager.dll"; "XblGameSave" = "XblGameSave.dll"; "XboxGipSvc" = "XboxGipSvc.dll"
+}
+
+$contatoreAnomalie = 0
+$anomalieLista = [System.Collections.Generic.List[PSCustomObject]]::new()
+$modificheRecenti = [System.Collections.Generic.List[string]]::new()
+
+foreach ($service in $services) {
+    $serviceName = $service.PSChildName
+
+    try {
+        $regKey = [Microsoft.Win32.RegistryKey]::OpenBaseKey(
+            [Microsoft.Win32.RegistryHive]::LocalMachine,
+            [Microsoft.Win32.RegistryView]::Registry64
+        ).OpenSubKey("SYSTEM\CurrentControlSet\Services\$serviceName")
+
+        if ($regKey) {
+            $internalMethod = $regKey.GetType().GetMethod(
+                "InternalGetSubKeyTimestamp",
+                [System.Reflection.BindingFlags]::NonPublic -bor [System.Reflection.BindingFlags]::Instance
+            )
+            if ($internalMethod) {
+                $lastWrite = [DateTime]::FromFileTime($internalMethod.Invoke($regKey, $null))
+            } else {
+                $lastWrite = (Get-Item "HKLM:\SYSTEM\CurrentControlSet\Services\$serviceName" -ErrorAction SilentlyContinue).LastWriteTime
+            }
+            $regKey.Close()
+
+            if ($lastWrite -and $lastWrite -ge $timeThreshold) {
+                $modificheRecenti.Add("$serviceName | $($lastWrite.ToString('HH:mm:ss dd/MM/yyyy'))")
+            }
+        }
+    } catch {}
+
+    $parametersPath = "$servicesPath\$serviceName\Parameters"
+    if (-not (Test-Path $parametersPath)) { continue }
+
+    $serviceDll = (Get-ItemProperty -Path $parametersPath -Name ServiceDll -ErrorAction SilentlyContinue).ServiceDll
+    if (-not $serviceDll) { continue }
+
+    $expandedPath = [System.Environment]::ExpandEnvironmentVariables($serviceDll)
+    if (-not (Split-Path $expandedPath -Parent)) {
+        $expandedPath = Join-Path "C:\Windows\System32" $expandedPath
+    }
+
+    $anomalie = @()
+    $fileItem = Get-Item -Path $expandedPath -ErrorAction SilentlyContinue
+
+    if (-not $fileItem) {
+        $anomalie += "FILE NOT FOUND"
+    } else {
+        $pathNorm = $expandedPath.ToLower()
+        $pathiLegittimi = @(
+            "$env:SystemRoot\system32",
+            "$env:SystemRoot\syswow64",
+            "$env:SystemRoot\system32\spool",
+            "$env:SystemRoot\winsxs"
+        )
+        $pathOk = $pathiLegittimi | Where-Object { $pathNorm.StartsWith($_.ToLower()) }
+        if (-not $pathOk) { $anomalie += "SUSPICIOUS PATH" }
+
+        $sig = Get-AuthenticodeSignature -FilePath $expandedPath -ErrorAction SilentlyContinue
+        if (-not $sig) {
+            $anomalie += "SIGNATURE NOT VERIFIABLE"
+        } elseif ($sig.Status -eq "NotSigned") {
+            $anomalie += "DLL NOT SIGNED"
+        } elseif ($sig.Status -ne "Valid") {
+            $anomalie += "INVALID SIGNATURE: $($sig.Status)"
+        } elseif ($sig.SignerCertificate.Subject -notlike "*Microsoft*") {
+            $anomalie += "NON-MICROSOFT SIGNATURE"
+        }
+
+        if ($knownMappings.ContainsKey($serviceName)) {
+            if ($fileItem.Name.ToLower() -ne $knownMappings[$serviceName].ToLower()) {
+                $anomalie += "DLL REPLACED (expected: $($knownMappings[$serviceName]))"
+            }
+        } else {
+            if ($fileItem.Name -notmatch "\.dll$") {
+                $anomalie += "ANOMALOUS EXTENSION: $($fileItem.Name)"
+            }
+        }
+    }
+
+    if ($anomalie.Count -gt 0) {
+        $contatoreAnomalie++
+        $dataModifica = if ($fileItem) { $fileItem.LastWriteTime.ToString("HH:mm:ss dd/MM/yyyy") } else { "N/A (file not found)" }
+        $itemBlock = [PSCustomObject]@{
+            Servizio = $serviceName
+            File     = $expandedPath
+            Modifica = $dataModifica
+            Stato    = ($anomalie -join " | ")
+        }
+        $anomalieLista.Add($itemBlock)
+    }
+}
+
+Write-Host "=============================================" -ForegroundColor DarkGray
+Write-Host " SERVICE SCANNER" -ForegroundColor Cyan
+Write-Host " User: $userName" -ForegroundColor Cyan
+Write-Host "=============================================" -ForegroundColor DarkGray
+Write-Host ""
+
+if ($contatoreAnomalie -gt 0) {
+    Write-Host "ANOMALIES DETECTED: $contatoreAnomalie" -ForegroundColor Red
+    Write-Host ""
+    foreach ($item in $anomalieLista) {
+        Write-Host "[$($item.Servizio)]" -ForegroundColor Yellow
+        Write-Host "  File     : $($item.File)"
+        Write-Host "  Modified : $($item.Modifica)"
+        Write-Host "  Status   : $($item.Stato)" -ForegroundColor Red
+        Write-Host ""
+    }
+} else {
+    Write-Host "No anomalies detected." -ForegroundColor Green
+    Write-Host ""
+}
+
+Write-Host "---------------------------------------------" -ForegroundColor DarkGray
+if ($modificheRecenti.Count -gt 0) {
+    Write-Host "REGISTRY MODIFIED IN LAST 60 MIN - SUSPICIOUS ($($modificheRecenti.Count)):" -ForegroundColor Yellow
+    foreach ($m in $modificheRecenti) {
+        Write-Host "  $m"
+    }
+} else {
+    Write-Host "No recent registry modifications (last 60 min)." -ForegroundColor Green
+}
+
+Write-Host "---------------------------------------------" -ForegroundColor DarkGray
+Write-Host "Scan: $(Get-Date -Format 'HH:mm:ss dd/MM/yyyy')" -ForegroundColor Cyan
+Write-Host ""
+
+Write-Host "Press any key to close..." -ForegroundColor Cyan
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
